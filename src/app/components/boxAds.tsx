530481e063e29df6ac4ebd70bf9f831e40a4bc8c
arthue de souza silva <arthurdesouza.silv@gmail.com>
2023-05-18 22:10:02 -0300

    add ads

--- src/app/components/boxAds.tsx
+++ src/app/components/boxAds.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { BlockList } from "net";
 
-export async function BoxAds() {
+export function BoxAds() {
     return (
         <>
-            
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5537187449610195"
+                crossOrigin="anonymous"></script>
+
+            <ins
+                className="bg-red-100 block h-100 w-100"
+                data-ad-client="ca-pub-5537187449610195"
+                data-ad-slot="9882629147"
+                data-adstest="on"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({ });
+            </script>
         </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
 import Image from 'next/image'
-import { BoxAds } from './components/boxAds'
+import {BoxAds} from './components/boxAds'
 
 export default function Home() {
-  return (
-    <main className="">
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5537187449610195"
-      ></script>
-
-      <ins id="adsbygoogle"
-        className="block bg-red-100"
-        data-ad-client="ca-pub-5537187449610195"
-        data-ad-slot="9882629147"
-        date-adstest="on"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({ });
-      </script>
+  return(
+    <main>
+      <h1>oi</h1>
+      <BoxAds/>
     </main>
   )
-}
+ }
