efea87674909ce3037504e068a96c17f57939c03
arthue de souza silva <arthurdesouza.silv@gmail.com>
2023-05-19 22:36:52 -0300

    at

--- src/app/components/boxAds.tsx
+++ src/app/components/boxAds.tsx
@@ -4,12 +4,11 @@ export function BoxAds() {
         <div className="mt-16">
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5537187449610195"
      crossOrigin="anonymous"></script>
-     
+
             <ins
                 className="block w-68 h-64"
                 data-ad-client="ca-pub-5537187449610195"
                 data-ad-slot="9882629147"
-                data-adstest="on"
                 data-ad-format="auto"
                 data-full-width-responsive="true"
             >
