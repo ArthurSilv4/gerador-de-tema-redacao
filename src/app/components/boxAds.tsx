31329cdf99342f82b7a18867e0c9254e6041c80d
arthue de souza silva <arthurdesouza.silv@gmail.com>
2023-05-19 22:28:58 -0300

    interface

--- src/app/components/boxAds.tsx
+++ src/app/components/boxAds.tsx
@@ -1,12 +1,12 @@
 
 export function BoxAds() {
     return (
-        <>
+        <div className="mt-16">
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5537187449610195"
                 crossOrigin="anonymous"></script>
 
             <ins
-                className="block w-64 h-64 bg-red-100"
+                className="block w-68 h-64 bg-red-100 "
                 data-ad-client="ca-pub-5537187449610195"
                 data-ad-slot="9882629147"
                 data-adstest="on"
@@ -18,6 +18,6 @@ export function BoxAds() {
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({ });
             </script>
-        </>
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,54 @@
-import Image from 'next/image'
-import {BoxAds} from './components/boxAds'
+'use client';
 
-export default function Home():JSX.Element {
-  return(
-    <main>
-      <h1>oi</h1>
-      <BoxAds/>
+import { useState } from 'react';
+import { BoxAds } from './components/boxAds'
+
+export default function Home(){
+
+  const temas = [
+    'Pandemia', 
+    'Racismo e Discriminação Social', 
+    'Racismo e Discriminação Social', 
+    'Preconceito Linguístico',
+  ];
+  const [temaAleatorio, setemaAleatorio] = useState('');
+
+  const escolherTemaAleatorio = () => {
+    const indiceAleatorio = Math.floor(Math.random() * temas.length);
+    const temaSelecionado = temas[indiceAleatorio];
+    setemaAleatorio(temaSelecionado);
+  };
+
+
+  return (
+    <main className="m-4 py-4 md:m-8 md:py-8">
+
+       <div className="bg-cyan-700 max-w-[600px] m-auto p-2 rounded-lg flex flex-col drop-shadow-lg border-double border-2 border-cyan-900 md:p-4">
+        
+          <div className="m-auto">
+            <h1 className="bg-cyan-900 p-2 rounded-lg text-center text-2xl font-extrabold drop-shadow-2xl md:text-4xl md:p-4">
+              Gerador de tema de redação
+            </h1>
+          </div>
+
+          <div className='m-auto my-36'>
+            <p className='bg-blue-950 p-4 rounded-3xl text-xs font-semibold drop-shadow-sm border-solid border-2 border-cyan-900 md:p-8 md:text-lg'>
+              {temaAleatorio}
+            </p>
+          </div>
+
+          <div className='m-auto pb-4 w-5/6 '>
+            <button className='bg-green-700 hover:bg-green-600 p-2 rounded-xl font-extrabold w-full md:p-4 md:text-3xl'
+              onClick={escolherTemaAleatorio}
+            >
+              GERAR TEMA ALEATÓRIO
+            </button>
+          </div>
+      
+      </div>
+
+      <BoxAds />
+      <BoxAds /> 
     </main>
   )
- }
+}
